f169658b7668094c8bc76f07d71f981ad462ebe8
Prabir Kumar Das <dasprabirkumar530@gmail.com>
2022-08-08 12:50:26 +0530

    Initial commit

---
+++ n/Spy Robot Project Files/SPY_ROBOT_CODE/SPY_ROBOT_CODE.ino
@@ -0,0 +1,146 @@
+  //XPERT MIND.
+//This program is used to control a spy robot using a app that communicates with Arduino through a bluetooth module.
+//Error Code Chart: Code 01; Turnradius is higher than Speed; Code 02; Speed is higher than 255;
+#include <Servo.h>
+#define in1 5 //L298n Motor Driver pins.
+#define in2 6
+#define in3 10
+#define in4 11
+Servo myservo;
+int command; //Int to store app command state.
+int Speed = 204; // 0 - 255.
+int Speedsec;
+int Turnradius = 40; //Set the radius of a turn, 0 - 255 Note:the robot will malfunction if this is higher than int Speed.
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  myservo.attach(3); //Set the Servo pin.
+  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    Stop(); //Initialize with motors stoped.
+    switch (command) {
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+      case 'G':
+        forwardleft();
+        break;
+      case 'I':
+        forwardright();
+        break;
+      case 'H':
+        backleft();
+        break;
+      case 'J':
+        backright();
+        break;
+      case '0':
+        Speed = 100;
+        break;
+      case '1':
+        Speed = 140;
+        break;
+      case '2':
+        Speed = 153;
+        break;
+      case '3':
+        Speed = 165;
+        break;
+      case '4':
+        Speed = 178;
+        break;
+      case '5':
+        Speed = 191;
+        break;
+      case '6':
+        Speed = 204;
+        break;
+      case '7':
+        Speed = 216;
+        break;
+      case '8':
+        Speed = 229;
+        break;
+      case '9':
+        Speed = 242;
+        break;
+      case 'q':
+        Speed = 255;
+        break;
+      case 'X':
+        zero();
+        break;
+      case 'x':
+        full();
+        break;
+    }
+    Speedsec = Turnradius; 
+  }
+}
+
+void forward() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speed);
+}
+
+void back() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speed);
+}
+
+void left() {
+  analogWrite(in3, Speed);
+  analogWrite(in2, Speed);
+}
+
+void right() {
+  analogWrite(in4, Speed);
+  analogWrite(in1, Speed);
+}
+void forwardleft() {
+  analogWrite(in1, Speedsec);
+  analogWrite(in3, Speed);
+}
+void forwardright() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speedsec);
+}
+void backright() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speedsec);
+}
+void backleft() {
+  analogWrite(in2, Speedsec);
+  analogWrite(in4, Speed);
+}
+
+void Stop() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
+void zero() {
+  myservo.write(0);  // To See Below through camera
+  delay(1500);
+ 
+}
+void full() {
+  myservo.write(140);  // To see in front by camera
+  delay(1500);
+}
